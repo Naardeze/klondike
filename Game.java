eaaa95e00ef1bde138a533e1fe35f9571630052a
Naardeze <141658644+Naardeze@users.noreply.github.com>
2024-10-27 02:52:00 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -90,7 +90,7 @@ public class Game extends JLabel implements MouseListener, Runnable {
         setOpaque(true);
         setBackground(new Color(-16726016));
         setHorizontalAlignment(JLabel.CENTER);
-        setFont(deck.getFont().deriveFont((float) CARD_HEIGHT / 3));
+        setFont(getFont().deriveFont((float) CARD_HEIGHT / 3));
         setPreferredSize(new Dimension(tableau[tableau.length - 1].getX() + tableau[tableau.length - 1].getWidth() + INSET, tableau[tableau.length - 1].getY() + tableau[tableau.length - 1].getHeight() + INSET));
 
         addMouseListener(this);
